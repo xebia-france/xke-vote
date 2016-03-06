07a6ec6934fa372cd030e46cf2e18455517e6127
Fatiha Bouad <fbouad@xebia.fr>
2016-03-06 17:03:10 +0100

    Prepare XKE 07/03

--- server/src/main/conf/slots.js
+++ server/src/main/conf/slots.js
@@ -1,98 +1,30 @@
 export const slotsData = [
   {
-    period: '13h45-15H45',
+    period: '10h-11h',
     talks: [
       {
         id: 1,
-        text: 'FP 105: type class - Hands On',
-        fondation: 'Craft',
+        text: 'Comment mener un projet Big Data agile?',
+        fondation: 'Agile',
         attendees: 0
       },
       {
         id: 2,
-        text: 'Linux Namespaces - Demo',
-        fondation: 'Devops',
-        attendees: 0
-      },
-      {
-        id: 3,
-        text: 'Hands-on: Intro à la 3D et realité virtuelle',
-        fondation: 'Divers',
-        attendees: 0
-      },
-      {
-        id: 4,
-        text: 'Le test n\'est pas (qu\')une histoire de testeur!' +
-        ' && JIRA Portfolio : présentation de la planification agile.',
-        fondation: 'Agile',
-        attendees: 0
-      },
-      {
-        id: 5,
-        text: 'Ansible hors des sentiers battus && Rundeck',
-        fondation: 'WeScale',
-        attendees: 0
-      }]
-  },
-  {
-    period: '15h45-16H45',
-    talks: [
-      {
-        id: 6,
-        text: 'Comment aider son product owner à prioriser son backlog ?',
-        fondation: 'Agile',
-        attendees: 0
-      },
-      {
-        id: 7,
-        text: ' les agents de la jam et Findtheflow.io',
+        text: 'Antlr 4 - Formal/Live Coding',
         fondation: 'Back',
         attendees: 0
       },
       {
-        id: 8,
-        text: ' Live coding AureliaJS',
-        fondation: 'Front',
+        id: 3,
+        text: 'Demo - Découvrez les APIs de La Poste',
+        fondation: 'Back',
         attendees: 0
       },
       {
-        id: 9,
-        text: 'REX sur Domotique',
-        fondation: 'IoT',
+        id: 4,
+        text: 'Rex- GoCD',
+        fondation: 'DevOps',
         attendees: 0
-      }]
-  },
-  {
-    period: '16h45-18H45',
-    talks: [
-      {
-        id: 10,
-        text: 'Rédiger des User Stories en Gherkin',
-        fondation: 'Agile',
-        attendees: 0
-      },
-      {
-        id: 11,
-        text: 'Bataille Navale en Micro Services - Hands-On',
-        fondation: 'Craft',
-        attendees: 0
-      },
-      {
-        id: 12,
-        text: 'Hands-On - R, ou comprendre avec quoi travaillent vos amis les Data Scientists',
-        fondation: 'Data',
-        attendees: 0
-      },
-      {
-        id: 13,
-        text: 'Hand\'s on Docker Swarm',
-        fondation: 'Devops',
-        attendees: 0
-      },
-      {
-        id: 14,
-        text: 'Demo - Ember 2.0',
-        fondation: 'Front',
-        attendees: 0
-      }]
+      }
+    ]
   }];
