abdf9f2d5a5915c18866b698f52e257f91e0898d
Fatiha Bouad <fbouad@xebia.fr>
2016-05-02 13:20:35 +0200

    update slots afternoon for XKE (02/05/16)

--- server/src/main/conf/slots.js
+++ server/src/main/conf/slots.js
@@ -1,35 +1,57 @@
 export const slotsData = [
   {
-    period: '10h45-11h40',
+    period: '14h35-16H30',
     talks: [
       {
         id: 1,
-        text: '[Demo] Kodo Kojo',
-        fondation: 'Divers',
+        text: '[Formal][Hands-On] Métrologie: quoi, pourquoi, comment?',
+        fondation: 'DevOps',
         attendees: 0
       },
       {
         id: 2,
-        text: '[REX] Lora/LoraWan',
-        fondation: 'Divers',
+        text: '[Hands-On] Sigfox',
+        fondation: 'IoT',
         attendees: 0
       },
       {
         id: 3,
-        text: 'Testing React & Redux',
-        fondation: 'Front',
+        text: '[Hands-On] Demistifying FP: Functionnal Error Handling',
+        fondation: 'Craft',
         attendees: 0
       },
       {
         id: 4,
-        text: '[BackToBasics] Les 5 questions con(tre) l\'agilité et comment y répondre',
+        text: '[Hands-On] Cassandra, LA base',
+        fondation: 'Back',
+        attendees: 0
+      }]
+  },
+  {
+    period: '16H40-18H30',
+    talks: [
+      {
+        id: 5,
+        text: 'Beer Game',
         fondation: 'Agile',
         attendees: 0
       },
       {
-        id: 5,
-        text: '[Demo] Tout est une question de style',
+        id: 6,
+        text: '[Hands-On][Demo] Live Dojo',
         fondation: 'Craft',
+        attendees: 0
+      },
+      {
+        id: 7,
+        text: '[Hands-On] Kafka',
+        fondation: 'Data',
+        attendees: 0
+      },
+      {
+        id: 8,
+        text: '[Formal] Le DevOps pour les nuls\n& [Hands-On] Vagrant',
+        fondation: 'Divers',
         attendees: 0
       }
     ]
