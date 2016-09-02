6ec7b61d32dc84ee246e77949c31b8aa7e3f549e
Fatiha Bouad <fbouad@xebia.fr>
2016-09-02 14:58:00 +0200

    prepare XKE Septembre

--- server/src/main/conf/slots.js
+++ server/src/main/conf/slots.js
@@ -1,90 +1,100 @@
 export const slotsData = {
-  am: [
+  am: [{
+    period: '9h50-10h45',
+    talks: [
+      {
+        id: 1,
+        text: '[Demo] GraphX et GraphFrame',
+        fondation: 'Data',
+        attendees: 0
+      },
+      {
+        id: 2,
+        text: '[Formal] Le déploiement continu rêvé chez Aldebaran',
+        fondation: 'Divers',
+        attendees: 0
+      },
+      {
+        id: 3,
+        text: '[REX] Angular 2: 10j pour coder une webapp de gestion toutes options  &&  [Formal] Objenious + Lora: découverte',
+        fondation: 'Divers',
+        attendees: 0
+      }
+    ]
+  },
     {
-      period: '10h15-11h10',
+      period: '10h50-11h45',
       talks: [
-        {
-          id: 1,
-          text: '[Formal] - Le Dark DAO: que c\'est il passé ? #blockchain',
-          fondation: 'Divers',
-          attendees: 0
-        },
-        {
-          id: 2,
-          text: '[Formal] - Ops Tooling - Debug from the other side',
-          fondation: 'DevOps',
-          attendees: 0
-        },
-        {
-          id: 3,
-          text: '[WeScale] Sentry logging',
-          fondation: 'Cloud',
-          attendees: 0
-        }
-      ]
-    }],
-  pm: [
-    {
-      period: '14h10-16H05',
-      talks: [
-        {
-          id: 1,
-          text: '[Hands on] - Concurrente programming with Go.',
-          fondation: 'Back',
-          attendees: 0
-        },
-        {
-          id: 2,
-          text: '[Hands-On] - Akka Stream (Scala)',
-          fondation: 'Back',
-          attendees: 0
-        },
-        
-        {
-          id: 3,
-          text: '[HandsOn] Demistifying Scala: The Future is Now. Full Async Programming.',
-          fondation: 'Craft',
-          attendees: 0
-        },
         {
           id: 4,
-          text: '[Hands On] Implementer les 4 niveaux de SAFe avec Jira et quelques Add On && [Formal] The Physical Web: A sneak peak into the future of IoT and mobility',
+          text: '[Formal] Comment reconnaître le bon du mauvais manager ?',
           fondation: 'Divers',
           attendees: 0
         },
         {
           id: 5,
-          text: '[WeScale] Designate: Openstack DNS as a Service && [Formal] The Physical Web: A sneak peak into the future of IoT and mobility',
+          text: '[Formal] NSD+Unbound, ou comment et pourquoi remplacer Bind',
           fondation: 'Divers',
+          attendees: 0
+        }, {
+          id: 6,
+          text: '[Formal] Sumo : Le continuous delivery qui envoie du lourd',
+          fondation: 'Divers',
+          attendees: 0
+        }
+      ]
+    }
+  ],
+  pm: [
+    {
+      period: '14h00-15H55',
+      talks: [
+        {
+          id: 1,
+          text: '[Hands-on] Introduction à TensorFlow',
+          fondation: 'Data',
+          attendees: 0
+        },
+        {
+          id: 2,
+          text: '[Formal] COAP le nouveau protocole IOT  &&  [Formal] GraphQL/Relay vs FalcorJS, un nouveau regard sur les architectures REST ?',
+          fondation: 'Divers',
+          attendees: 0
+        },
+
+        {
+          id: 3,
+          text: '[REX] Design Sprint && [Hands-on] Comment annoncer une date de release fixe',
+          fondation: 'Agile',
           attendees: 0
         }
       ]
     },
     {
-      period: '16h10-18h05',
+      period: '16h00-17h55',
       talks: [
         {
-          id: 1,
-          text: '[Hands-On] - Implementing front propagation in Scala',
+          id: 4,
+          text: '[Debate] Agile et architecture, vous avez 2 heures',
+          fondation: 'Agile',
+          attendees: 0
+        },
+        {
+          id: 5,
+          text: '[Demo][Live Coding] Scala en 24h',
           fondation: 'Back',
           attendees: 0
         },
         {
-          id: 2,
-          text: '[Hands-On] - Sigfox, un cran plus loin, avec une application dans le cloud.',
-          fondation: 'IoT',
+          id: 6,
+          text: '[Hands On] Introduction à React Native',
+          fondation: 'Mobile',
           attendees: 0
         },
         {
-          id: 3,
-          text: '[REX] Un lac, des chevaux et des hommes',
+          id: 7,
+          text: '[Formal] Architecture CDN (REX Canal+) && [Formal] Scrapy, initiation au scrapping web en pytho',
           fondation: 'Divers',
-          attendees: 0
-        },
-        {
-          id: 4,
-          text: '[WeScale] StackStorm',
-          fondation: 'Cloud',
           attendees: 0
         }
       ]
