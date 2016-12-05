d64731684bab9e366102036be3dcc7c71573ec4c
Antoine MICHAUD <antoinemichaud34@gmail.com>
2016-12-05 06:24:48 +0100

    2016-12-05

--- server/src/main/conf/slots.js
+++ server/src/main/conf/slots.js
@@ -1,99 +1,82 @@
 export const slotsData = {
   am: [{
-    period: '9h50-10h45',
+    period: '9h45-11h40',
     talks: [
       {
         id: 1,
-        text: '[Demo] GraphX et GraphFrame',
+        text: 'Search on Hadoop',
         fondation: 'Data',
         attendees: 0
       },
       {
         id: 2,
-        text: '[Formal] Le déploiement continu rêvé chez Aldebaran',
-        fondation: 'Divers',
+        text: 'Conteneurs 101',
+        fondation: 'DevOps',
         attendees: 0
       },
       {
         id: 3,
-        text: '[REX] Angular 2: 10j pour coder une webapp de gestion toutes options  &&  [Formal] Objenious + Lora: découverte',
+        text: 'Réalisation d\'un jeu sous Unity',
         fondation: 'Divers',
         attendees: 0
       }
     ]
-  },
-    {
-      period: '10h50-11h45',
-      talks: [
-        {
-          id: 4,
-          text: '[Formal] Comment reconnaître le bon du mauvais manager ?',
-          fondation: 'Divers',
-          attendees: 0
-        },
-        {
-          id: 5,
-          text: '[Formal] NSD+Unbound, ou comment et pourquoi remplacer Bind',
-          fondation: 'Divers',
-          attendees: 0
-        }, {
-          id: 6,
-          text: '[Formal] Sumo : Le continuous delivery qui envoie du lourd',
-          fondation: 'Divers',
-          attendees: 0
-        }
-      ]
-    }
+  }
   ],
   pm: [
     {
-      period: '14h00-15H55',
+      period: '14h30-16h25',
       talks: [
         {
           id: 1,
-          text: '[Hands-on] Introduction à TensorFlow',
-          fondation: 'Data',
+          text: 'IBM Bluemix',
+          fondation: 'Cloud',
           attendees: 0
         },
         {
           id: 2,
-          text: '[Hands-on] Introduction à React Native',
-          fondation: 'Mobile',
+          text: 'Logmatic + Technologie for a better world',
+          fondation: 'Divers',
           attendees: 0
         },
-
         {
           id: 3,
-          text: '[REX] Design Sprint && [Hands-on] Comment annoncer une date de release fixe',
-          fondation: 'Agile',
+          text: 'Rock-IT + Comment nager avec agilité dans un Datalake',
+          fondation: 'Divers',
+          attendees: 0
+        },
+        {
+          id: 4,
+          text: 'Pimp my PHP + Métriques de qualité sous Android + Menlook',
+          fondation: 'Divers',
           attendees: 0
         }
       ]
     },
     {
-      period: '16h00-17h55',
+      period: '16h30-18h230',
       talks: [
         {
-          id: 4,
-          text: '[Debate] Agile et architecture, vous avez 2 heures',
-          fondation: 'Agile',
-          attendees: 0
-        },
-        {
           id: 5,
-          text: '[Demo][Live Coding] Scala en 24h',
+          text: 'Akka Stream',
           fondation: 'Back',
           attendees: 0
         },
         {
           id: 6,
-          text: '[Formal] COAP le nouveau protocole IOT[Hands On] &&  [Formal] GraphQL/Relay vs FalcorJS, un nouveau regard sur les architectures REST ?',
-          fondation: 'Divers',
+          text: 'Comment (vraiment) tester une application Android',
+          fondation: 'Back',
           attendees: 0
         },
         {
           id: 7,
-          text: '[Formal] Architecture CDN (REX Canal+) && [Formal] Scrapy, initiation au scrapping web en python',
+          text: 'Rex PMU Data + La fin de la hiérarchie n\'est pas celle du manager',
+          fondation: 'Divers',
+          attendees: 0
+        },
+        {
+          id: 8,
+          text: 'POC Michelin + Slideware 101',
           fondation: 'Divers',
           attendees: 0
         }
